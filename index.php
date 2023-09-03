9849862934a7c044b31ad31304e45e1cf63b6d8b
Gabriele Grosse <ggross7577@gmail.com>
2023-09-03 12:49:26 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2>Hello World!</h2>";
+        ?>
+    </body>
+</html>
